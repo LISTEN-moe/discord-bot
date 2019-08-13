ee9c5d95c53a4daebd57c52f2f2120b271c3dc3e
iCrawl <icrawltogo@gmail.com>
2019-08-13 16:42:36 +0200

    fix: logger

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -4,10 +4,11 @@ import * as DailyRotateFile from 'winston-daily-rotate-file';
 export const logger = createLogger({
 	format: format.combine(
 		format.errors({ stack: true }),
+		format.label({ label: 'BOT' }),
 		format.timestamp({ format: 'YYYY/MM/DD HH:mm:ss' }),
 		format.printf((info: any): string => {
 			const { timestamp, label, level, message, ...rest } = info;
-			return `[${timestamp}][${label}][${level.toUpperCase()}]}: ${message}${Object.keys(rest).length ? `\n${JSON.stringify(rest, null, 2)}` : ''}`;
+			return `[${timestamp}][${label}][${level.toUpperCase()}]: ${message}${Object.keys(rest).length ? `\n${JSON.stringify(rest, null, 2)}` : ''}`;
 		})
 	),
 	transports: [
